c53176fb6677dabf0e47de0b4631fb32a4746243
Ashwin Reddy <ashwin.reddy338@gmail.com>
2014-05-09 21:47:51 -0700

    Update lang.js

---
+++ lang.js
@@ -0,0 +1,4 @@
+// As of right now, we are not sure which language to use
+// However, we think JavaScript (JS) will be a good way to go
+// This file will probably undergo important changes as it changes along the way
+// Right now, there will be nothing here....
