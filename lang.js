533b573274f8de8bf93e90620ad8a73680e7378e
roseguan <rose.guan@gmail.com>
2014-05-12 10:01:06 -0700

    Add credits.

--- lang.js
+++ lang.js
@@ -2,3 +2,32 @@
 // However, we think JavaScript (JS) will be a good way to go
 // This file will probably undergo important changes as it changes along the way
 // Right now, there will be nothing here....
+
+
+
+
+// THE LANGUAGE GOES HERE
+
+
+
+
+// Harker Programming Club
+// HPL Language
+
+// Credits:
+// CLUB MEMBERS
+  // Ryan Adolf
+  // Ayush Alag
+  // Rose Guan
+  // Austin Hou
+  // Shafieen Ibrahim
+  // David Melisso
+  // Rithvik Panchapakesan
+  // Ayush Pancholy
+  // Nishant Ravi
+  // Akshay Ravoor
+  // Keval Shah
+
+// SPECIAL THANKS
+  // Michael Schmidt
+  // The Harker School
